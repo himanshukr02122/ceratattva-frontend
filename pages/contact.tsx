f66172a932e733ef7b1b5bed8a863116d4fd64f7
unknown <himanshukumar02122@gmail.com>
2023-10-17 00:07:26 +0530

    update: started working on contact-us page

---
+++ pages/contact.tsx
@@ -0,0 +1,12 @@
+import ContactUsBanner from '@/components/sections/ContactUsBanner'
+import React from 'react'
+
+const ContactUs = () => {
+  return (
+    <>
+      <ContactUsBanner/>
+    </>
+  )
+}
+
+export default ContactUs
